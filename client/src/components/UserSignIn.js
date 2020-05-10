3bcafcaf923f0dc0a34136ac2a3e3d1363f432ca
Daniel Grigo <daniel.grigo@freenet.de>
2020-05-10 22:12:12 +0200

    Added Context

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
+import {Consumer} from '../Context';
 
 class UserSignIn extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     searchInput: ""
-  //   }
+  constructor(props) {
+    super(props);
+    this.state= {
+      user: {}
+    };
+  }
+
 
   render(){
     return(
-      <div className="bounds">
-        <div className="grid-33 centered signin">
-          <h1>Sign In</h1>
-          <div>
-            <form>
-              <div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value=""></div>
-              <div><input id="password" name="password" type="password" className="" placeholder="Password" value=""></div>
-              <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
-            </form>
-          </div>
-          <p>&nbsp;</p>
-          <p>Don't have a user account? <a href="sign-up.html">Click here</a> to sign up!</p>
-        </div>
-      </div>
-  )}
+      <Consumer>
+        {context => {
+          let updateInput = context.actions.updateInput.bind(this);
+          let handleRequest = context.actions.handleRequest
+          return(
+            <div className="bounds">
+              <div className="grid-33 centered signin">
+                <h1>Sign In</h1>
+                <div>
+                  <form onSubmit={(e) => {e.preventDefault(); handleRequest(`/users`, "get", this.state)}}>
+                    <div>
+                      <input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" onChange= {updateInput}/>
+                    </div>
+                    <div>
+                      <input id="password" name="password" type="password" className="" placeholder="Password" onChange= {updateInput}/>
+                    </div>
+                    <div className="grid-100 pad-bottom">
+                      <button className="button" type="submit">Sign In</button>
+                      <button className="button button-secondary" onClick={(e) => {e.preventDefault(); this.props.history.push("/")}}>Cancel</button>
+                    </div>
+                  </form>
+                </div>
+                <p>&nbsp;</p>
+                <p>Don't have a user account? <a href="/signup">Click here</a> to sign up!</p>
+              </div>
+            </div>
+          )
+        }}
+      </Consumer>
+    )}
 }
 
   export default UserSignIn;
--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,32 +1,55 @@
 import React, { Component } from 'react';
+import {Consumer} from '../Context';
 
 class UserSignUp extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     searchInput: ""
-  //   }
+  constructor(props) {
+    super(props);
+    this.state= {
+      user: {}
+    };
+  }
+
   render(){
     return(
-      <div className="bounds">
-        <div className="grid-33 centered signin">
-          <h1>Sign Up</h1>
-          <div>
-            <form>
-              <div><input id="firstName" name="firstName" type="text" className="" placeholder="First Name" value=""></div>
-              <div><input id="lastName" name="lastName" type="text" className="" placeholder="Last Name" value=""></div>
-              <div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value=""></div>
-              <div><input id="password" name="password" type="password" className="" placeholder="Password" value=""></div>
-              <div><input id="confirmPassword" name="confirmPassword" type="password" className="" placeholder="Confirm Password"
-                  value=""></div>
-              <div className="grid-100 pad-bottom"><button className="button" type="submit">Sign Up</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button></div>
-            </form>
-          </div>
-          <p>&nbsp;</p>
-          <p>Already have a user account? <a href="sign-in.html">Click here</a> to sign in!</p>
-        </div>
-      </div>
-  )}
-}
+      <Consumer>
+        {context => {
+          let updateInput = context.actions.updateInput.bind(this);
+          let handleRequest = context.actions.handleRequest
+          return(
+            <div className="bounds">
+              <div className="grid-33 centered signin">
+                <h1>Sign Up</h1>
+                <div>
+                  <form onSubmit={(e) => {e.preventDefault(); handleRequest(`/users`, "post", this.state)}}>
+                    <div>
+                      <input id="firstName" name="firstName" type="text" className="" placeholder="First Name" onChange= {updateInput} />
+                    </div>
+                    <div>
+                      <input id="lastName" name="lastName" type="text" className="" placeholder="Last Name" onChange= {updateInput} />
+                    </div>
+                    <div>
+                      <input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" onChange= {updateInput} />
+                    </div>
+                    <div>
+                      <input id="password" name="password" type="password" className="" placeholder="Password" onChange= {updateInput}/>
+                    </div>
+                    <div>
+                      <input id="confirmPassword" name="confirmPassword" type="password" className="" placeholder="Confirm Password" onChange= {updateInput} />
+                      </div>
+                    <div className="grid-100 pad-bottom">
+                      <button className="button" type="submit">Sign Up</button>
+                      <button className="button button-secondary" onClick={(e) => {e.preventDefault(); this.props.history.push("/")}}>Cancel</button>
+                    </div>
+                  </form>
+                </div>
+                <p>&nbsp;</p>
+                <p>Already have a user account? <a href="/signin">Click here</a> to sign in!</p>
+              </div>
+            </div>
+          )
+        }}
+      </Consumer>
+    )}
+  }
 
   export default UserSignUp;
