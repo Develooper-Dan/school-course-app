f0be46c2313b4f6b0978772d563484719a039812
Daniel Grigo <daniel.grigo@freenet.de>
2020-05-15 11:17:49 +0200

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
 import {NavLink} from "react-router-dom"
 
-class Header extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     searchInput: ""
-  //   }
-  render(){
-    return(
-  <div className="header">
-    <div className="bounds">
-      <h1 className="header--logo">Courses</h1>
-      <nav>
-        <NavLink to='/signup'>Sign Up</NavLink>
-        <NavLink to='/signin'>Sign In</NavLink>
-      </nav>
+function Header(){
+  return(
+    <div className="header">
+      <div className="bounds">
+        <h1 className="header--logo">Courses</h1>
+        <nav>
+          <NavLink to='/signup'>Sign Up</NavLink>
+          <NavLink to='/signin'>Sign In</NavLink>
+        </nav>
+      </div>
     </div>
-  </div>
-  )}
+  )
 }
 
   export default Header;
