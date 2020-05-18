cedf282c0bc28454a9a263059bb727d14ee2a718
Daniel Grigo <daniel.grigo@freenet.de>
2020-05-18 08:35:58 +0200

    finalized

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -2,22 +2,21 @@ import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
 import {Consumer} from '../Context';
 
-export default function PrivateRoute(WrappedComponent, ...props){
+export default function PrivateRoute({component: Component, ...props}){
   return (
     <Consumer>
       {context => (
-        <Route {...props} render={({location}) => {
-          console.log(context.authenticatedUser)
+        <Route {...props} render={props => {
           if(context.authenticatedUser){
             return (
-              <WrappedComponent context= {context}/>
+              <Component context= {context} routeProps= {props} />
             )
           } else {
             return(
               <Redirect
                 to={{
                   pathname: "/signin",
-                  state: { from: location }
+                  state: { from: props.location }
                 }}
               />
             )}
