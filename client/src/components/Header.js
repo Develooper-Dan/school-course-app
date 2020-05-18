06864d3bbb79393aa35889226d8bb1ee7b549ffc
Daniel Grigo <daniel.grigo@freenet.de>
2020-05-18 14:11:01 +0200

    Conditionally rendering nav elements

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {NavLink} from "react-router-dom"
+import {Consumer} from '../Context';
 
 function Header(){
   return(
-    <div className="header">
-      <div className="bounds">
-        <h1 className="header--logo">Courses</h1>
-        <nav>
-          <NavLink to='/signup'>Sign Up</NavLink>
-          <NavLink to='/signin'>Sign In</NavLink>
-        </nav>
-      </div>
-    </div>
+    <Consumer>
+      {context => {
+        let navElements;
+        if(context.authenticatedUser){
+          let {firstName, lastName} = context.authenticatedUser;
+          navElements = (
+            <nav>
+              <span>Welcome, {firstName} {lastName}!</span>
+              <NavLink to='/signout'>Sign Out</NavLink>
+            </nav>
+          )
+        } else {
+          navElements = (
+            <nav>
+              <NavLink to='/signup'>Sign Up</NavLink>
+              <NavLink to='/signin'>Sign In</NavLink>
+            </nav>
+          )
+        }
+
+        return(
+          <div className="header">
+            <div className="bounds">
+              <h1 className="header--logo">Courses</h1>
+                {navElements}
+            </div>
+          </div>
+        )
+      }}
+    </Consumer>
   )
 }
 
-  export default Header;
+export default Header;
