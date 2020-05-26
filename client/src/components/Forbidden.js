5c77e369af087f490893066c89d0b6908443467e
Daniel Grigo <daniel.grigo@freenet.de>
2020-05-26 21:38:16 +0200

    Removed location.state

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 // This component gets mounted if the user somehow requests a non-existing site
 const Forbidden = (props) =>{
-  let {history, location} = props
-  console.log(props)
-  let { from } = location.state || {from}
+  let {history} = props
   return (
     <div className="bounds">
       <h1>Access denied</h1>
-      <p>The access to the site or resource at <strong>{from.pathname}</strong> is restricted to registered users only. !</p>
+      <p>You're not the registered owner of this course!</p>
       <button type="button" onClick={() => history.goBack()}>Back</button>
     </div>
   )
