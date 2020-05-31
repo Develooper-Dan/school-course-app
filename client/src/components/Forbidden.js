21f3d27301bcd144e33978ce2c7ad058254f8dc9
Daniel Grigo <daniel.grigo@freenet.de>
2020-05-31 22:12:02 +0200

    Changed div className

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Forbidden = (props) =>{
   let {history} = props
   return (
-    <div className="bounds">
+    <div className="error">
       <h1>Access denied</h1>
       <p>You're not the registered owner of this course!</p>
       <button type="button" onClick={() => history.goBack()}>Back</button>
--- client/src/components/UnhandledError.js
+++ client/src/components/UnhandledError.js
@@ -3,7 +3,7 @@ import React from 'react';
 // This component gets mounted if a serverside error occurs
 const UnhandledError = (props) =>{
   return (
-    <div className="bounds">
+    <div className="error">
       <h1>Error</h1>
       <p>An unexpected error occured. Sorry!</p>
     </div>
